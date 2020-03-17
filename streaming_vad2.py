6d36b38cc9b9356f525c83a53e4d376b6523df45
Myslav488 <grabowski.przemek168@gmail.com>
2020-03-17 23:47:03 +0100

    I am commitin once again

--- streaming_vad2.py
+++ streaming_vad2.py
@@ -8,14 +8,11 @@ import subprocess
 import os
 
 # globalne tablice do wyswietlania zlaczonych sygnalow
-wholerun1 = [0] * 40000
-wholerun2 = [0] * 40000
-audio1 = np.ndarray((8000,))
-audio2 = np.ndarray((8000,))
-wekt_zazn0 = np.ndarray((8000,))
-wekt_zazn2 = np.ndarray((8000,))
-
+wholerun1 = [0] * 24000
+wholerun2 = [0] * 24000
 # globalna wartosc rms do normalizacji
+zazn_przd = 0
+zazn_tyl = 0
 rms1 = 10**8
 
 if __name__ == '__main__':
@@ -28,35 +25,33 @@ if __name__ == '__main__':
         print("startRecordingArecord()> rec_proc pid= " + str(rec_proc.pid))
 
         # wczytywanie pliku z nagraniem
-        Fs, audio0 = wavfile.read('input_read1.wav', mmap=True)
+        Fs, audio = wavfile.read('input_read1.wav', mmap=False)
+
         # filtr antyaliasingowy
         filtr = scipy.signal.firwin2(1024, [0, 0.167, 0.183, 1], [1, 1, 0, 0])
-        audio0 = scipy.signal.convolve(audio0, filtr, mode='full', method='auto')
-        audio0 = audio0[:Fs]
+        audio = scipy.signal.convolve(audio, filtr, mode='full', method='auto')
+        audio = audio[:Fs]
 
         # flitr przeciwkoprzydzwiekowi
         filtr = scipy.signal.firwin(1023, 160, fs=Fs, pass_zero=False)
-        audio0 = scipy.signal.convolve(audio0, filtr, mode='full', method='auto')
-        audio0 = audio0[:Fs]
+        audio = scipy.signal.convolve(audio, filtr, mode='full', method='auto')
+        audio = audio[:Fs]
 
         # decymacja
-        audio0 = audio0[0::6]
+        audio1 = audio[0::6]
         Fs /= 6
 
-        global audio1
-        global wekt_zazn0
-        global wekt_zazn2
          # normalizacja do rms sygnalu
         rms = np.sqrt(np.mean(audio1 ** 2))
         global rms1
         rms1 = 0.8 * rms1 + 0.2 * rms
-        print(rms)
+        # print("Wartosc rms do normalizacji: ", rms1)
         audio1 = audio1 / rms1
         # filtr preemfazy
-        audio1 = np.append(audio1[0], audio1[1:] - 0.95 * audio1[:-1])
+        audio2 = np.append(audio1[0], audio1[1:] - 0.95 * audio1[:-1])
 
         # transformata sygnalu
-        audiofft = fft(audio1)
+        audiofft = fft(audio2)
         audiofft = (2 / Fs) * np.abs(audiofft[:int(Fs) // 2])
 
         # prog mocy calego sygnalu (wyznaczany empirycznie)
@@ -65,24 +60,23 @@ if __name__ == '__main__':
         # dlugosc okna w ms * 1000 / Fs
         winlen = 10 * 8
         # wektor mocy sygnalu
-        pow_vec = np.ones(len(audio1))
+        pow_vec = np.ones(len(audio2))
 
         # petla obliczenia mocy sygnalu w okanach
-        for cnt in range(0, len(audio1), winlen):
-            tempsamp = audio1[cnt:cnt + winlen]
+        for cnt in range(0, len(audio2), winlen):
+            tempsamp = audio2[cnt:cnt + winlen]
             pow_vec[cnt:cnt + winlen] *= np.mean(np.abs(fft(tempsamp)))
 
         # wektor wyroznienia sygnalu z informacja glosowa
-        wektor_zazn = np.zeros(len(audio1))
-        print(np.info(wektor_zazn))
-        wektor_zazn = wekt_zazn0
-        print(np.info(wektor_zazn))
+        wektor_zazn = np.zeros(len(audio2))
         stan_wysoki = 2
 
         # wyroznienie fragmentow sygnalu ktorych moc widmowa przekracza wyznaczony prog
         for cnt in range(0, len(wektor_zazn)):
             if pow_vec[cnt] > prog:
                 wektor_zazn[cnt] = stan_wysoki
+
+
 
         # licznik aktywnosci sygnalu
         znak = 0
@@ -100,6 +94,8 @@ if __name__ == '__main__':
                 znak = 0
 
         # petla zaznaczenia 200 ms aktywnosci przed i po sygnale
+        global zazn_przd
+        global zazn_tyl
         cnt = 0
         while cnt < len(wektor_zazn):
             if stan_wysoki == wektor_zazn[cnt] and stan_wysoki != wektor_zazn[cnt - 1] and cnt > 200 * 8:
@@ -108,28 +104,37 @@ if __name__ == '__main__':
             elif stan_wysoki == wektor_zazn[cnt] and stan_wysoki != wektor_zazn[cnt - 1]:
                 # print("\nOperacja 2")
                 wektor_zazn[0:cnt] = stan_wysoki
-            elif stan_wysoki == wektor_zazn[cnt - 1] and stan_wysoki != wektor_zazn[cnt] and len(audio1) - cnt > 200 * 8:
+                zazn_tyl = 300*8 - cnt
+            elif stan_wysoki == wektor_zazn[cnt - 1] and stan_wysoki != wektor_zazn[cnt] and len(audio2) - cnt > 200 * 8:
                 # print("\nOperacja 3")
                 wektor_zazn[cnt:cnt + 300 * 8] = stan_wysoki
                 cnt += 300 * 8 + 2
             elif stan_wysoki == wektor_zazn[cnt - 1] and stan_wysoki != wektor_zazn[cnt]:
                 # print("\nOperacja 4")
-                wektor_zazn[cnt:len(audio1)] = stan_wysoki
+                wektor_zazn[cnt:len(audio2)] = stan_wysoki
                 cnt += 300 * 8
+                zazn_przd = len(audio2) - cnt + 300*8
             cnt += 1
 
+        #sprawdz czy ma cos do zaznaczenia z poprzedniej sekundy sygnalu
+        if 0 != zazn_przd:
+            wektor_zazn[:zazn_przd] = stan_wysoki
+            zazn_przd = 0
 
         # sklejanie sygnalow
-        global audio2
         global wholerun1
         wholerun1 = wholerun1[8000:]
         wholerun1 = np.append(wholerun1, audio2)
-        audio2 = audio1
-        audio1 = audio0
+
+        zzz = 0
+        for cnt in range(0, len(wholerun1)):
+            if 0 == wholerun1[cnt]:
+                zzz += 1
+        print("HAHAHAHAHAHHAHAHAHAHAHHAHAHAH ", zzz)
+
         global wholerun2
         wholerun2 = wholerun2[8000:]
-        wholerun2 = np.append(wholerun2, wekt_zazn2)
-        wekt_zazn2 = wektor_zazn
+        wholerun2 = np.append(wholerun2, wektor_zazn)
 
         # os czasu
         x_values = np.arange(0, len(wholerun1), 1) / float(Fs)
