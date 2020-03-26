96bdcc01fb47fb0a83cf000b890ed1d2451cc035
Myslav488 <grabowski.przemek168@gmail.com>
2020-03-26 17:23:50 +0100

    Some extra filters experimenting.(26-03-2020)

--- streaming_spectrum.py
+++ streaming_spectrum.py
@@ -7,6 +7,8 @@ from matplotlib.animation import FuncAnimation
 from scipy.fftpack import fft
 import scipy.signal
 
+if os.path.exists('input_read1.wav'):
+    os.remove('input_read1.wav')
 
 def animate(i):
     proc_args = ['arecord', '-D', 'plughw:1,0', '-d', '1', '-c1', '-M', '-r', '48000', '-f', 'S32_LE', '-t', 'wav',
@@ -14,8 +16,12 @@ def animate(i):
     rec_proc = subprocess.Popen(proc_args, shell=False, preexec_fn=os.setsid)
     print("startRecordingArecord()> rec_proc pid= " + str(rec_proc.pid))
 
-    # read the input file
-    Fs, audio = wavfile.read('input_read1.wav', mmap=True)
+    # wczytywanie pliku z nagraniem
+    if os.path.exists('input_read1.wav'):
+        Fs, audio = wavfile.read('input_read1.wav', mmap=False)
+    else:
+        Fs = 48000
+        audio = [0] * Fs
 
     # antyaliasing filter
     filtr = scipy.signal.firwin2(1024, [0, 0.167, 0.183, 1], [1, 1, 0, 0])
@@ -43,17 +49,25 @@ def animate(i):
     ##Filtracja dzwieku wentylatora
     # zapisywanie widma sygnalu
     # if os.path.exists("widmo.csv"):
-    #    os.remove("widmo.csv")
+    #   os.remove("widmo.csv")
     # audiofft.tofile("widmo.csv", sep=';', format='%10.5f')
 
     # wczytywanie widma sygnalu
-    # audiox = np.fromfile("widmo.csv", sep=';')
+    audiox = np.fromfile("widmo.csv", sep=';')
     # print(np.info(audiox))
-    # audiofft -= audiox
+    '''
+    for cnt in range(270,290):
+        if np.argmax(audiox[270:290]) == np.argmax(audiofft[270:290]):
+            audiofft -= audiox
+            break
+        else:
+            audiofft = np.append(audiofft[1:], 0)
+            continue
+    '''
     # freq axis
-    x_values = np.arange(0, len(audiofft), 1)
+    x_values = np.arange(0, len(audiox), 1)
     plt.cla()
-    plt.plot(x_values, audiofft, label='Signal')
+    plt.plot(x_values, audiox, label='Signal')
     plt.legend(loc='upper left')
     plt.tight_layout()
 
